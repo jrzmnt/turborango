6579372dd98a613a8ebdcacc07841e1e0a2b49b9
Juarez Monteiro dos Santos Júnior <jrzmonteiro@gmail.com>
2015-06-02 14:08:39 -0300

    Tarefa C#

---
+++ src/TurboRango/TurboRango.Dominio/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+namespace TurboRango.Dominio
+{
+    enum Categoria
+    {
+        [Description("Comum")]
+        COMUM,
+        [Description("Cozinha Natural")]
+        COZINHA_NATURAL,
+        [Description("Cozinha Mexicana")]
+        COZINHA_MEXICANA,
+        [Description("Churrascaria")]
+        CHURRASCARIA,
+        [Description("Cozinha Japonesa")]
+        COZINHA_JAPONESA,
+        [Description("Fast Food")]
+        FASTFOOD,
+        [Description("Pizzaria")]
+        PIZZARIA
+    }
+}
