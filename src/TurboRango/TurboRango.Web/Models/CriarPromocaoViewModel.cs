67ca11f58f7585f0da86a0e237eee26fbbe2f433
juarezmonteiro <jrzmonteiro@gmail.com>
2015-06-11 16:58:50 -0300

    feat(model: CriarPromocaoViewModel)

---
+++ src/TurboRango/TurboRango.Web/Models/CriarPromocaoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TurboRango.Web.Models
+{
+    public class CriarPromocaoViewModel
+    {
+        public int RestauranteId { get; set; }
+        public Decimal ValorPromocao { get; set; }
+        public DateTime TerminoPromocao { get; set; }
+    }
+}
